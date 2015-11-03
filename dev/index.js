76c405a8294df2fd8bd3984c5ddc797241e6e044
Ron Raithatha <ronaksraithatha@gmail.com>
2015-11-02 21:34:09 -0500

    babel 6-fied, and a samlpe. Needs some more work

---
+++ dev/index.js
@@ -0,0 +1,20 @@
+var loginBoilerplate = require("../dist/index");
+
+var userInfo = {
+    "username": "Test",
+    "email": "test@gmail.com",
+    "password": "password"
+};
+
+//var user = loginBoilerplate.createUser(userInfo)
+//    .then(user => console.log("user in: ", user))
+//    .catch(err => console.log(err));
+
+var user = loginBoilerplate.createUser(userInfo).then(function (user){
+                console.log("user in: ", user);
+                return user;
+            }).catch(function(err) {
+                console.log(err);
+            });
+
+console.log("user out: ", user);
